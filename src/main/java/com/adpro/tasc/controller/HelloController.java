9ed215b16c8490e2cfe96c4797fa667f4f74993d
Muhammad Zahran Agung Dewantoro <muhammad.zahran@ui.ac.id>
2021-05-27 15:40:17 +0700

    add navbar

--- src/main/java/com/adpro/tasc/controller/HelloController.java
+++ src/main/java/com/adpro/tasc/controller/HelloController.java
@@ -1,23 +1,9 @@
 package com.adpro.tasc.controller;
 
-import com.adpro.tasc.appointment.db.model.Appointment;
-import com.adpro.tasc.appointment.db.model.AppointmentRequest;
-import com.adpro.tasc.appointment.db.model.Course;
 import com.adpro.tasc.user.db.dao.UserDAO;
-import com.adpro.tasc.user.db.model.Role;
-import com.adpro.tasc.user.db.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Controller
 public class HelloController {
@@ -40,4 +26,18 @@ public class HelloController {
         return "register";
     }
 
+    @GetMapping("/home-admin")
+    public String adminPage() {
+        return "homeAdmin";
+    }
+
+    @GetMapping("/home-TA")
+    public String TAPage() {
+        return "homeTA";
+    }
+
+    @GetMapping("/home-student")
+    public String StudentPage() {
+        return "homeStudent";
+    }
 }
