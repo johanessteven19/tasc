b77f2e1ae9f5ec290b5eabf678f48ec1393ab669
Achmad Afriza <achmad.afriza@gmail.com>
2021-05-28 19:43:00 +0700

    Added method to create schedule

--- src/main/java/com/adpro/tasc/appointment/db/dao/ScheduleDAO.java
+++ src/main/java/com/adpro/tasc/appointment/db/dao/ScheduleDAO.java
@@ -3,8 +3,13 @@ package com.adpro.tasc.appointment.db.dao;
 import com.adpro.tasc.appointment.db.model.Schedule;
 import com.adpro.tasc.appointment.db.model.Slot;
 import com.adpro.tasc.user.db.model.AcademicUser;
+import com.adpro.tasc.user.db.model.User;
 
 public interface ScheduleDAO {
     Schedule getUserSchedule(AcademicUser user);
     Schedule getUserScheduleByDay(AcademicUser user, Slot.Day day);
+
+    void createSchedule(Schedule schedule);
+    void deleteSchedule(int id);
+    void deleteSchedule(User user);
 }
